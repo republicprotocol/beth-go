ede7d46b73ae8451a0f3744dcadea2e065020549
Divya Mary <divyakoshy05@gmail.com>
2018-10-03 15:03:33 +1000

    Revert "utils, conn: fixed documentation and refactored conn.go"
    This reverts commit 44a3a2331801c547b8e9a93ee0e7e39da6b7fda9.

--- conn.go
+++ conn.go
@@ -2,6 +2,8 @@ package eth
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"math/big"
 	"time"
 
@@ -9,172 +11,84 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/republicprotocol/eth-go/utils"
 )
 
-// Client will have a connection to an ethereum client (specified by the url)
-type Client struct {
-	ethClient *ethclient.Client
-	url       string
+// Conn contains the client and the contracts deployed to it
+type Conn struct {
+	Client *ethclient.Client
 }
 
 // Connect to an infura network (Supported networks: mainnet and kovan).
-func Connect(url string) (Client, error) {
+func Connect(network string) (Conn, error) {
+	uri := ""
 
-	ethClient, err := ethclient.Dial(url)
-	if err != nil {
-		return Client{}, err
+	switch network {
+	case "mainnet":
+		uri = "https://mainnet.infura.io"
+	case "kovan":
+		uri = "https://kovan.infura.io"
+	default:
+		return Conn{}, fmt.Errorf("cannot connect to %s: unsupported", network)
 	}
 
-	return Client{
-		ethClient: ethClient,
-		url:       url,
+	ethclient, err := ethclient.Dial(uri)
+	if err != nil {
+		return Conn{}, err
+	}
+
+	return Conn{
+		Client: ethclient,
 	}, nil
 }
 
-// WaitMined waits for tx to be mined on the blockchain.
+// PatchedWaitMined waits for tx to be mined on the blockchain.
 // It stops waiting when the context is canceled.
-func (client *Client) WaitMined(ctx context.Context, tx *types.Transaction) (*types.Receipt, error) {
-	return bind.WaitMined(ctx, client.ethClient, tx)
+//
+// TODO: THIS DOES NOT WORK WITH PARITY, WHICH SENDS A TRANSACTION RECEIPT UPON
+// RECEIVING A TX, NOT AFTER IT'S MINED
+func (conn *Conn) PatchedWaitMined(ctx context.Context, tx *types.Transaction) (*types.Receipt, error) {
+	receipt, err := bind.WaitMined(ctx, conn.Client, tx)
+	if err != nil {
+		return nil, err
+	}
+	// if receipt.Status != types.ReceiptStatusSuccessful {
+	// 	return receipt, errors.New("transaction reverted")
+	// }
+	return receipt, nil
 }
 
-// Get will perform a read-only transaction on the ethereum blockchain.
-func (client *Client) Get(
-	ctx context.Context,
-	callOpts *bind.CallOpts,
-	f func(*bind.CallOpts) (interface{}, error),
-) (val interface{}, err error) {
+// PatchedWaitDeployed waits for a contract deployment transaction and returns the on-chain
+// contract address when it is mined. It stops waiting when ctx is canceled.
+//
+// TODO: THIS DOES NOT WORK WITH PARITY, WHICH SENDS A TRANSACTION RECEIPT UPON
+// RECEIVING A TX, NOT AFTER IT'S MINED
+func (conn *Conn) PatchedWaitDeployed(ctx context.Context, tx *types.Transaction) (common.Address, error) {
+	return bind.WaitDeployed(ctx, conn.Client, tx)
 
-	sleepDurationMs := time.Duration(1000)
-
-	// Keep retrying until the read-only transaction succeeds or until context
-	// times out
-	for {
-		select {
-		case <-ctx.Done():
-			if err == nil {
-				return val, ctx.Err()
-			}
-			return
-		default:
-		}
-
-		if val, err = f(callOpts); err == nil {
-			return
-		}
-
-		// If transaction errors, wait for sometime before retrying
-		select {
-		case <-ctx.Done():
-			if err == nil {
-				return val, ctx.Err()
-			}
-			return
-		case <-time.After(sleepDurationMs * time.Millisecond):
-		}
-
-		// Increase delay for next round but saturate at 30s
-		sleepDurationMs = time.Duration(float64(sleepDurationMs) * 1.6)
-		if sleepDurationMs > 30000 {
-			sleepDurationMs = 30000
-		}
-	}
 }
 
 // BalanceOf returns the ethereum balance of the addr passed.
-func (client *Client) BalanceOf(
-	ctx context.Context,
-	addr common.Address,
-	callOpts *bind.CallOpts,
-) (*big.Int, error) {
-
-	val, err := client.Get(ctx, callOpts, func(*bind.CallOpts) (interface{}, error) {
-		return client.ethClient.BalanceAt(ctx, addr, nil)
-	})
-	if err != nil {
-		return big.NewInt(0), err
-	}
-	return val.(*big.Int), nil
+func (conn *Conn) BalanceOf(addr common.Address) (*big.Int, error) {
+	return conn.Client.BalanceAt(context.Background(), addr, nil)
 }
 
-// GetBlockNumberByTxHash retrieves tx's block number using the tx hash.
-func (client *Client) GetBlockNumberByTxHash(ctx context.Context, hash string) (*big.Int, error) {
-
-	type Result struct {
-		BlockNumber string `json:"blockNumber,omitempty"`
-	}
-	type JSONResponse struct {
-		Result Result `json:"result,omitempty"`
-	}
-	var data JSONResponse
-
-	var jsonStr = `{"jsonrpc":"2.0","method":"eth_getTransactionByHash","params":["` + hash + `"],"id":1}`
-
-	// Keep retrying until a block number is returned or until context times out
+// Get will perform
+func (conn *Conn) Get(ctx context.Context, f func(opts *bind.CallOpts) (interface{}, error)) (interface{}, error) {
+	sleepFor := time.Duration(1000)
 	for {
+
 		select {
 		case <-ctx.Done():
-			return nil, ctx.Err()
+			return nil, errors.New("context timed out")
 		default:
 		}
 
-		response, err := utils.SendRequest(ctx, client.url, jsonStr, data)
-		if err != nil {
-			continue
+		if val, err := f(&bind.CallOpts{}); err == nil {
+			return val, err
 		}
-		data = response.(JSONResponse)
+		time.Sleep(sleepFor)
 
-		if data.Result == (Result{}) || data.Result.BlockNumber == "" {
-			select {
-			case <-ctx.Done():
-				return nil, ctx.Err()
-			case <-time.After(5 * time.Millisecond):
-			}
-			continue
-		}
-		break
+		// Increase delay for next round.
+		sleepFor = time.Duration(float64(sleepFor) * 1.6)
 	}
-
-	return utils.HexToBigInt(data.Result.BlockNumber)
-}
-
-// GetCurrentBlockNumber will retrieve the current block that is confirmed by
-// infura.
-func (client *Client) GetCurrentBlockNumber(ctx context.Context) (*big.Int, error) {
-	type Result struct {
-		Number string `json:"number,omitempty"`
-	}
-	type JSONResponse struct {
-		Result Result `json:"result,omitempty"`
-	}
-	var data JSONResponse
-
-	var jsonStr = `{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}`
-
-	// Keep retrying until a block number is returned or until context times out
-	for {
-		select {
-		case <-ctx.Done():
-			return nil, ctx.Err()
-		default:
-		}
-
-		response, err := utils.SendRequest(ctx, client.url, jsonStr, data)
-		if err != nil {
-			continue
-		}
-		data = response.(JSONResponse)
-
-		if data.Result == (Result{}) || data.Result.Number == "" {
-			select {
-			case <-ctx.Done():
-				return nil, ctx.Err()
-			case <-time.After(5 * time.Millisecond):
-			}
-			continue
-		}
-		break
-	}
-
-	return utils.HexToBigInt(data.Result.Number)
 }
