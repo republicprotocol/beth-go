ca3bfafda52be42de2599599bb3415f0e74038a9
Divya Mary <divyakoshy05@gmail.com>
2018-10-03 15:03:40 +1000

    Revert "eth: Updated names of methods"
    This reverts commit 9107f672d3be5d280db3a0a35f0dfee6d4e34dec.

--- eth.go
+++ eth.go
@@ -5,36 +5,34 @@ import (
 	"crypto/ecdsa"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 )
 
-// UserAccount which is associated with a private key can read/write to the
+// Account which is associated with a private key can read/write to the
 // ethereum blockchain.
-type UserAccount interface {
+type Account interface {
 	// Address returns the ethereum address of the account holder.
 	Address() common.Address
 
 	// Transfer sends the specified value of Eth to the given address.
-	Transfer(ctx context.Context, to common.Address, value *big.Int, confirmBlocks int64) error
+	Transfer(ctx context.Context, to common.Address, value *big.Int) error
 
 	// Transact performs a write operation on the Ethereum blockchain. It will
 	// first conduct a preConditionCheck and if the check passes, it will
 	// repeatedly execute the transaction followed by a postConditionCheck,
 	// until the transaction passes and the postConditionCheck returns true.
-	Transact(ctx context.Context, preConditionCheck func() bool, f func(bind.TransactOpts) (*types.Transaction, error), postConditionCheck func(ctx context.Context) bool, confirmBlocks int64) error
+	Transact(preConditionCheck func() bool, f func() (types.Transaction, error), postConditionCheck func() bool) error
 }
 
-// NewUserAccount returns an Account object associated with the given private key.
-func NewUserAccount(network string, privateKey *ecdsa.PrivateKey) (UserAccount, error) {
-	return NewAccount(network, privateKey)
+// NewAccount returns an Account object associated with the given private key.
+func NewAccount(network string, privateKey *ecdsa.PrivateKey) (Account, error) {
+	return NewEthAccount(network, privateKey)
 }
 
-// NewClient returns a new connection to ethereum network. This connection can be
+// NewConn returns a new connection to ethereum network. This connection can be
 // used for performing read-only operations on smart contracts and for reading
 // balance of ethereum addresses.
-func NewClient(network string) (Client, error) {
+func NewConn(network string) (Conn, error) {
 	return Connect(network)
 }
