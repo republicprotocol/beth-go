46378ac3c8faa4a36850e6ee999353bd1ed1ea26
Jaz Gulati <jaskaran.g15@gmail.com>
2018-11-30 09:49:28 +1100

    addressbook: added token addresses

--- addressbook.go
+++ addressbook.go
@@ -11,7 +11,28 @@ type Address struct {
 	Address string
 }
 
-var MainnetAddresses = []Address{}
+var MainnetAddresses = []Address{
+	Address{
+		Name:    "DGX",
+		Address: "0x4f3AfEC4E5a3F2A6a1A411DEF7D7dFe50eE057bF",
+	},
+	Address{
+		Name:    "TUSD",
+		Address: "0x8dd5fbCe2F6a956C3022bA3663759011Dd51e73E",
+	},
+	Address{
+		Name:    "REN",
+		Address: "0x21C482f153D0317fe85C60bE1F7fa079019fcEbD",
+	},
+	Address{
+		Name:    "ZRX",
+		Address: "0xE41d2489571d322189246DaFA5ebDe1F4699F498",
+	},
+	Address{
+		Name:    "OMG",
+		Address: "0xd26114cd6EE289AccF82350c8d8487fedB8A0C07",
+	},
+}
 
 var RopstenAddresses = []Address{
 	Address{
@@ -54,8 +75,24 @@ var KovanAddresses = []Address{
 		Address: "0x2218fa20c33765e7e01671ee6aaca75fbaf3a974",
 	},
 	Address{
-		Name:    "SwapperdWBTC",
-		Address: "0x2218fa20c33765e7e01671ee6aaca75fbaf3a974",
+		Name:    "DGX",
+		Address: "0x932F4580B261e9781A6c3c102133C8fDd4503DFc",
+	},
+	Address{
+		Name:    "TUSD",
+		Address: "0x525389752ffe6487d33EF53FBcD4E5D3AD7937a0",
+	},
+	Address{
+		Name:    "REN",
+		Address: "0x2CD647668494c1B15743AB283A0f980d90a87394",
+	},
+	Address{
+		Name:    "ZRX",
+		Address: "0x6EB628dCeFA95802899aD3A9EE0C7650Ac63d543",
+	},
+	Address{
+		Name:    "OMG",
+		Address: "0x66497ba75dD127b46316d806c077B06395918064",
 	},
 }
 
